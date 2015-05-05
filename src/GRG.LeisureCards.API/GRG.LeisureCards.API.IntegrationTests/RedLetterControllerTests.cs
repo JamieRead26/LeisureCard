6dfd53a0243f241eb422c8c3a21c1d9e92b7dab2
myles-mcdonnell <mcdonnell.myles@gmail.com>
2015-05-05 23:08:03 +0100

    Introduced session mgmt

--- src/GRG.LeisureCards.API/GRG.LeisureCards.API.IntegrationTests/RedLetterControllerTests.cs
+++ src/GRG.LeisureCards.API/GRG.LeisureCards.API.IntegrationTests/RedLetterControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Net;
 using System.Reflection;
 using GRG.LeisureCards.Data;
 using GRG.LeisureCards.Model;
@@ -19,6 +20,20 @@ namespace GRG.LeisureCards.API.IntegrationTests
 
 
         [Test]
+        public void FindByKeyword_401()
+        {
+            var client = new RestClient(Config.BaseAddress);
+
+            var request = new RestRequest("RedLetter/Find/{keyword}", Method.GET);
+            request.AddParameter("keyword", "amphibious");
+            request.AddHeader("accepts", "application/json");
+
+            var response = client.Execute<List<RedLetterProductSummary>>(request);
+
+            Assert.AreEqual(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Test]
         public void FindByKeyword()
         {
             var client = new RestClient(Config.BaseAddress);
@@ -26,6 +41,7 @@ namespace GRG.LeisureCards.API.IntegrationTests
             var request = new RestRequest("RedLetter/Find/{keyword}", Method.GET);
             request.AddParameter("keyword", "amphibious");
             request.AddHeader("accepts", "application/json");
+            request.AddHeader("SessionToken", GetSessionToken());
 
             var response = client.Execute<List<RedLetterProductSummary>>(request);
 
@@ -40,8 +56,24 @@ namespace GRG.LeisureCards.API.IntegrationTests
             var request = new RestRequest("RedLetter/Get/{id}", Method.GET);
             request.AddParameter("id", "5850");
             request.AddHeader("accepts", "application/json");
+            request.AddHeader("SessionToken", GetSessionToken());
 
             Assert.IsNotNull(client.Execute<RedLetterProductSummary>(request));
+        }
+
+        public string GetSessionToken()
+        {
+            var client = new RestClient(Config.BaseAddress);
+
+            var request = new RestRequest("LeisureCard/Login/{code}", Method.GET);
+            request.AddParameter("code", "Registered");
+            request.AddHeader("accepts", "application/json");
+
+            var response = client.Execute<LeisureCardRegistrationResponse>(request);
+
+            Assert.AreEqual("Ok", response.Data.Status);
+
+            return response.Data.SessionToken;
         }
     }
 }

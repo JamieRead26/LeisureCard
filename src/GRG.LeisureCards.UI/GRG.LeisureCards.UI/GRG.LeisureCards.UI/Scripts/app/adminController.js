46b38238a26b6de5f180228a5bf289055a634333
jamie <jamie@sourceshaper.com>
2015-05-13 14:33:36 +0100

    admin reports

--- src/GRG.LeisureCards.UI/GRG.LeisureCards.UI/GRG.LeisureCards.UI/Scripts/app/adminController.js
+++ src/GRG.LeisureCards.UI/GRG.LeisureCards.UI/GRG.LeisureCards.UI/Scripts/app/adminController.js
@@ -12,17 +12,58 @@ adminController.factory('GetLoginHistory', function ($resource, config) {
     return $resource(config.apiUrl + '/Reports/GetLoginHistory/:from/:to');
 });
 
-adminController.controller('AdminController', function ($scope, GetLoginHistory) {
+adminController.controller('AdminController', function ($scope, GetLoginHistory, GetCardActivationHistory, GetSelectedOfferHistory) {
 
     $scope.global.slideshow = [];
+    $scope.reports_card_activation = [];
+    $scope.reports_offers_claimed = [];
+    $scope.reports_card_usage = [];
+    $scope.report_type = 'card_activation';
 
-    var to = new Date(2000, 1, 1);
-    var day = to.getDate();
-    var month = to.getMonth();
-    var year = to.getFullYear();
-  
-    GetLoginHistory.get({ from: '1/1/2000 00:00:00', to: '1/1/2001 00:00:00' }, function (data) {
-        debugger;
-    });
+    $scope.get_report = function (d) {
+        
+        $scope.reports_card_activation = [];
+        $scope.reports_offers_claimed = [];
+        $scope.reports_card_usage = [];
+        $scope.report_error = null;
+
+        var reg = new RegExp(/^(\d{1,2})-(\d{1,2})-(\d{4})$/);
+        if (!reg.test($scope.from_date) || !reg.test($scope.to_date)) {
+            return $scope.report_error = 'From and To dates must match format dd-mm-yyyy';
+        }
+
+        var search_data = { from: $scope.from_date, to: $scope.to_date }
+
+        if ($scope.report_type == 'card_activation') {
+            GetCardActivationHistory.get(search_data, function (data) {
+                $scope.reports_card_activation = data.$values;
+
+                if ($scope.reports_card_activation.length == 0) {
+                    return $scope.report_error = 'No results to show.';
+                }
+            });
+        }
+        else if($scope.report_type == 'offers_claimed'){
+            GetSelectedOfferHistory.get(search_data, function (data) {
+
+                $scope.reports_offers_claimed = data.$values;
+
+                if ($scope.reports_offers_claimed.length == 0) {
+                    return $scope.report_error = 'No results to show.';
+                }
+            });
+        }
+        else if ($scope.report_type == 'card_usage') {
+            GetLoginHistory.get(search_data, function (data) {
+                debugger;
+                $scope.reports_card_usage = data.$values;
+
+                if ($scope.reports_card_usage.length == 0) {
+                    return $scope.report_error = 'No results to show.';
+                }
+            });
+        }
+
+    };
 
 });

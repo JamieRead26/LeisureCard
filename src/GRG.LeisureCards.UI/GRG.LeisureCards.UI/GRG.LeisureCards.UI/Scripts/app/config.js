e7530fd672744b51cb4cf815e63fc2b03ff7a1e3
jamie <jamie@sourceshaper.com>
2015-05-15 14:47:21 +0100

    config for grg

--- src/GRG.LeisureCards.UI/GRG.LeisureCards.UI/GRG.LeisureCards.UI/Scripts/app/config.js
+++ src/GRG.LeisureCards.UI/GRG.LeisureCards.UI/GRG.LeisureCards.UI/Scripts/app/config.js
@@ -1,6 +1,6 @@
 ﻿app.constant('config', {
     googleAPiKey: 'AIzaSyAvl80GCl1HRw1H_FmBIGlLk2VPcV5R5Cg',
-    tenant: 'base',
+    tenant: 'grg',
     apiUrl: '//localhost:1623' //-//localhost:1623
 });
 

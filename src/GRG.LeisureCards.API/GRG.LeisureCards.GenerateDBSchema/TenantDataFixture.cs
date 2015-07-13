317ebb7f456d74c613034009e55131672cbf9a58
myles-mcdonnell <mcdonnell.myles@gmail.com>
2015-07-13 11:00:42 +0100

    PDF Docs WIP

--- src/GRG.LeisureCards.API/GRG.LeisureCards.GenerateDBSchema/TenantDataFixture.cs
+++ src/GRG.LeisureCards.API/GRG.LeisureCards.GenerateDBSchema/TenantDataFixture.cs
@@ -6,12 +6,14 @@ namespace GRG.LeisureCards.GenerateDBSchema
     public class TenantDataFixture : DataFixture
     {
         public Tenant GRG { get; private set; }
+        public Tenant NPower { get; private set; }
 
         public override object[] GetEntities(IFixtureContainer fixtureContainer)
         {
             GRG = new Tenant { TenantKey = "GRG", Name = "Grass Roots Group", Active = true, Domain = "leisurecards.grg.com" };
+            NPower = new Tenant { TenantKey = "NPower", Name = "NPower", Active = true, Domain = "leisurecards.npower.com" };
 
-            return new[] { GRG };
+            return new[] { GRG, NPower };
         }
     }
 }
--- src/GRG.LeisureCards.API/GRG.LeisureCards.PDFDocuments/NPower/HtmlTemplates.cs
+++ src/GRG.LeisureCards.API/GRG.LeisureCards.PDFDocuments/NPower/HtmlTemplates.cs
@@ -3,5 +3,10 @@
     public class HtmlTemplates : Global.HtmlTemplates
     {
         public override string TenantKey { get { return "NPower"; } }
+
+        protected override string TwoForOneVoucherContentResourceKey
+        {
+            get { return "GRG.LeisureCards.PDFDocuments.NPower.TwoForOneVoucher.html"; }
+        }
     }
 }

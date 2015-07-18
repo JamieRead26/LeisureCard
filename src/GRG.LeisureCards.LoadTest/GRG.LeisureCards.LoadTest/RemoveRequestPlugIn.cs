26b9d3c4472150db2d3fb2751b248cf808a6800f
myles-mcdonnell <mcdonnell.myles@gmail.com>
2015-07-18 19:10:58 +0100

    Public site web test all areas

---
+++ src/GRG.LeisureCards.LoadTest/GRG.LeisureCards.LoadTest/RemoveRequestPlugIn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.WebTesting;
+
+namespace GRG.LeisureCards.LoadTest
+{
+
+
+    public class RemoveRequestPlugIn : WebTestPlugin
+    {
+        public override void PostRequest(object sender, PostRequestEventArgs e)
+        {
+            List<WebTestRequest> remove = new List<WebTestRequest>();
+            foreach (WebTestRequest dependent in e.Request.DependentRequests)
+            {
+                if (dependent.Url.Contains("CategoryKey"))
+                {
+                remove.Add(dependent);
+                }
+            }
+            foreach (WebTestRequest dependent in remove)
+            {
+                e.Request.DependentRequests.Remove(dependent);
+            }
+        }
+    }
+}
